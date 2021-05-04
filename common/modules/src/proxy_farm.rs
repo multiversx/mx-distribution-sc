a20063bf6ce459b1f42daca88a253bdcf79b4ed7
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-05-04 16:26:10 +0300

    claimRewardsProxy

--- common/modules/src/proxy_farm.rs
+++ common/modules/src/proxy_farm.rs
@@ -14,11 +14,13 @@ use elrond_wasm::{contract_call, only_owner, require, sc_error, sc_try};
 
 const SIMULATE_ENTER_FARM_GAS_PRICE: u64 = 100000000;
 const SIMULATE_EXIT_FARM_GAS_PRICE: u64 = 100000000;
+const SIMULATE_CLAIM_REWARDS_GAS_PRICE: u64 = 100000000;
 const ENTER_FARM_GAS_PRICE: u64 = 60000000;
 const EXIT_FARM_GAS_PRICE: u64 = 60000000;
 
 const ENTER_FARM_FUNC_NAME: &[u8] = b"enterFarm";
 const EXIT_FARM_FUNC_NAME: &[u8] = b"exitFarm";
+const CLAIM_REWARDS_FUNC_NAME: &[u8] = b"claimRewards";
 
 #[derive(TopEncode, TopDecode, TypeAbi)]
 pub struct WrappedFarmTokenAttributes {
@@ -164,6 +166,71 @@ pub trait ProxyFarmModule {
         Ok(())
     }
 
+    #[payable("*")]
+    #[endpoint(claimRewardsProxy)]
+    fn claim_rewards_proxy(&self, farm_address: Address) -> SCResult<()> {
+        sc_try!(self.global_operation().require_not_ongoing());
+        sc_try!(self.require_is_intermediated_farm(&farm_address));
+
+        let a = 1;
+        let a = 1;
+        let token_nonce = self.call_value().esdt_token_nonce();
+        let (amount, token_id) = self.call_value().payment_token_pair();
+        require!(amount != 0, "Paymend amount cannot be zero");
+        require!(
+            token_id == self.token_id().get(),
+            "Should only be used with wrapped farm tokens"
+        );
+
+        // Read info about wrapped farm token and then burn it.
+        let wrapped_farm_token_attrs = sc_try!(self.get_attributes(&token_id, token_nonce));
+        let farm_token_id = wrapped_farm_token_attrs.farm_token_id;
+        let farm_token_nonce = wrapped_farm_token_attrs.farm_token_nonce;
+        self.burn_tokens(&token_id, token_nonce, &amount);
+
+        // Simulate an exit farm and get the returned tokens.
+        let exit_farm_result =
+            self.simulate_exit_farm(&farm_address, &farm_token_id, farm_token_nonce, &amount);
+        let lp_token_returned = exit_farm_result.0;
+        let reward_token_returned = exit_farm_result.1;
+
+        // Simulate an enter farm and get the returned tokens.
+        let enter_farm_result = self.simulate_enter_farm(
+            &farm_address,
+            &lp_token_returned.token_id,
+            &lp_token_returned.amount,
+        );
+        let new_farm_token_id = enter_farm_result.token_id;
+        let new_farm_token_nonce = enter_farm_result.token_nonce;
+        let new_farm_token_total_amount = enter_farm_result.amount;
+
+        // Do the actual claiming of rewards.
+        sc_try!(self.actual_exit_farm(&farm_address, &farm_token_id, farm_token_nonce, &amount));
+
+        // Send the reward to the caller.
+        let caller = self.blockchain().get_caller();
+        self.asset().send_tokens(
+            &reward_token_returned.token_id,
+            &reward_token_returned.amount,
+            &caller,
+        );
+
+        // Create new Wrapped tokens and send them.
+        let new_wrapped_farm_token_attributes = WrappedFarmTokenAttributes {
+            farm_token_id: new_farm_token_id,
+            farm_token_nonce: new_farm_token_nonce,
+            farmed_token_id: wrapped_farm_token_attrs.farmed_token_id,
+            farmed_token_nonce: wrapped_farm_token_attrs.farmed_token_nonce,
+        };
+        self.create_and_send_wrapped_farm_token(
+            &new_wrapped_farm_token_attributes,
+            &new_farm_token_total_amount,
+            &caller,
+        );
+
+        Ok(())
+    }
+
     fn burn_tokens(&self, token: &TokenIdentifier, nonce: Nonce, amount: &BigUint) {
         self.send().esdt_nft_burn(
             self.blockchain().get_gas_left(),
@@ -211,17 +278,16 @@ pub trait ProxyFarmModule {
         attributes: &WrappedFarmTokenAttributes,
         amount: &BigUint,
     ) {
-        self.send()
-            .esdt_nft_create::<WrappedFarmTokenAttributes>(
-                self.blockchain().get_gas_left(),
-                token_id.as_esdt_identifier(),
-                amount,
-                &BoxedBytes::empty(),
-                &BigUint::zero(),
-                &H256::zero(),
-                &attributes,
-                &[BoxedBytes::empty()],
-            );
+        self.send().esdt_nft_create::<WrappedFarmTokenAttributes>(
+            self.blockchain().get_gas_left(),
+            token_id.as_esdt_identifier(),
+            amount,
+            &BoxedBytes::empty(),
+            &BigUint::zero(),
+            &H256::zero(),
+            &attributes,
+            &[BoxedBytes::empty()],
+        );
 
         self.increase_nonce();
     }
@@ -254,7 +320,11 @@ pub trait ProxyFarmModule {
         );
         contract_call!(self, farm_address.clone(), FarmContractProxy)
             .simulateEnterFarm(lp_token_id.clone(), amount.clone())
-            .execute_on_dest_context_custom_range(gas_limit, |_, after| (after-1, after), self.send())
+            .execute_on_dest_context_custom_range(
+                gas_limit,
+                |_, after| (after - 1, after),
+                self.send(),
+            )
     }
 
     fn simulate_exit_farm(
@@ -310,6 +380,29 @@ pub trait ProxyFarmModule {
             amount,
             gas_limit,
             EXIT_FARM_FUNC_NAME,
+            &ArgBuffer::new(),
+        );
+        match result {
+            Result::Ok(()) => Ok(()),
+            Result::Err(_) => sc_error!("Failed to transfer to pair"),
+        }
+    }
+
+    fn actual_claim_rewards(
+        &self,
+        farm_address: &Address,
+        farm_token_id: &TokenIdentifier,
+        farm_token_nonce: Nonce,
+        amount: &BigUint,
+    ) -> SCResult<()> {
+        let gas_limit = core::cmp::min(self.blockchain().get_gas_left(), CLAIM_REWARDS_FUNC_NAME);
+        let result = self.send().direct_esdt_nft_execute(
+            farm_address,
+            farm_token_id.as_esdt_identifier(),
+            farm_token_nonce,
+            amount,
+            gas_limit,
+            CLAIM_REWARDS_FUNC_NAME,
             &ArgBuffer::new(),
         );
         match result {
